0f688f1770dd68e75245cb93589d1dfa7ae1a11d
Kethlyn Diniz <kethdiniz06@gmail.com>
2022-08-04 10:35:32 -0300

    avanço aula 03.08

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/Teste.java
@@ -9,13 +9,37 @@ public class Teste {
 
 	public static void main(String[] args) {
 		
-		ArrayList lista = new ArrayList();
+		ArrayList<Conta> lista = new ArrayList<Conta>();
 		
 		Conta cc = new ContaCorrente(22, 11);
 		lista.add(cc);
 		
 		Conta cc2 = new ContaCorrente(22, 22);
 		lista.add(cc2);
+		
+		System.out.println(lista.size());
+		Conta ref = (Conta) lista.get(0);
+		System.out.println(ref.getNumero());
+		
+		lista.remove(0);
+		
+		System.out.println("Tamanho " + lista.size());
+		
+		Conta cc3 = new ContaCorrente(22, 111);
+		lista.add(cc3);
+		
+		Conta cc4 = new ContaCorrente(22, 222);
+		lista.add(cc4);
+		
+		for(int i = 0; i < lista.size(); i++) {
+			System.out.println(lista.get(i));
+		}
+		
+		System.out.println("____________________");
+		
+		for(Object oRef : lista) { //para cada elemento do tipo object dessa lista faça{}
+			System.out.println(oRef);
+		}
 		
 	}
 
